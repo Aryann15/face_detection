64c8335335d0b231d7beaeb8f6aa268ca206025c
Aryann15 <rajlysm15@gmail.com>
2023-07-06 16:00:33 +0530

    model complete

---
+++ main.py
@@ -0,0 +1,19 @@
+import cv2 as cv
+
+img = cv.imread('Photos/group2  .jpeg')
+cv.imshow('literally me', img)
+
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY )
+
+haar_cascade = cv.CascadeClassifier('haar_face.xml')
+
+faces_rect = haar_cascade.detectMultiScale(gray,scaleFactor=1.1, minNeighbors=3)
+
+print(f'There are {len(faces_rect)} faces')
+
+for(x,y,w,h) in faces_rect:
+    cv.rectangle(img,(x,y),(x+w,y+h),(0,255,0),thickness= 2)
+
+cv.imshow('Detected faces',img)
+
+cv.waitKey(0)
