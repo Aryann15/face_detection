7a18234366b107f1a37560b5cf2ccee9c5ab1d5e
Aryann15 <rajlysm15@gmail.com>
2023-07-06 16:05:36 +0530

    tuning value of min Neighbours so as to get max efficiency

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 import cv2 as cv
 
-img = cv.imread('Photos/group2  .jpeg')
-cv.imshow('literally me', img)
+img = cv.imread('Photos/group2.jpeg')
+cv.imshow('original photo', img)
 
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY )
 
 haar_cascade = cv.CascadeClassifier('haar_face.xml')
 
-faces_rect = haar_cascade.detectMultiScale(gray,scaleFactor=1.1, minNeighbors=3)
-
+faces_rect = haar_cascade.detectMultiScale(gray,scaleFactor=1.1, minNeighbors=2)
 print(f'There are {len(faces_rect)} faces')
 
 for(x,y,w,h) in faces_rect:
